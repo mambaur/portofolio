61cc75e74c6b3a567d4fb6a9db68d3f743f2f23f
Mambaur <alwiroziq1998@gmail.com>
2021-06-29 00:39:54 +0700

    update skill data

---
+++ src/components/contents/skill/SkillData.jsx
@@ -0,0 +1,43 @@
+
+// Image
+import flutter from '../../../assets/technology/flutter.png';
+import laravel from '../../../assets/technology/laravel.png';
+
+const SkillData = [
+    {
+        key: 'laravel',
+        title: 'Laravel Framework',
+        image: laravel,
+        content: [
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi sequi hic recusandae dolor ut culpa voluptate aspernatur saepe ex maxime ipsa ipsum voluptas adipisci itaque officia mollitia odio, voluptates tempora!',
+            'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia accusamus quo dolorem voluptas et, corporis deleniti. Odio, eligendi quisquam ducimus cumque modi libero repellat veritatis aspernatur, temporibus ullam maxime a quam alias. Cum cumque amet, cupiditate blanditiis nam vero, earum, porro debitis eaque officia at maxime. Vitae, debitis! Reiciendis, accusantium!',
+            'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta at placeat nemo minima laboriosam quod eligendi quam, pariatur laborum ab voluptate. Suscipit velit excepturi odio ratione aspernatur impedit voluptatem voluptates aut minima adipisci, quis tenetur temporibus saepe vel similique expedita rerum accusamus eum est debitis asperiores animi atque, quod odit! Atque deleniti itaque voluptates nemo.',
+        ],
+        desc: {
+            author : 'Laravel Community',
+            initial_release: 'Alpha (v0.1)',
+            platform: 'Android, iOS',
+            license: 'LV BSD License'
+        },
+        documentation: 'http://doc.com'
+    },
+    {
+        key: 'flutter',
+        title: 'Flutter Framework',
+        image: flutter,
+        content: [
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi sequi hic recusandae dolor ut culpa voluptate aspernatur saepe ex maxime ipsa ipsum voluptas adipisci itaque officia mollitia odio, voluptates tempora!',
+            'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia accusamus quo dolorem voluptas et, corporis deleniti. Odio, eligendi quisquam ducimus cumque modi libero repellat veritatis aspernatur, temporibus ullam maxime a quam alias. Cum cumque amet, cupiditate blanditiis nam vero, earum, porro debitis eaque officia at maxime. Vitae, debitis! Reiciendis, accusantium!',
+            'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta at placeat nemo minima laboriosam quod eligendi quam, pariatur laborum ab voluptate. Suscipit velit excepturi odio ratione aspernatur impedit voluptatem voluptates aut minima adipisci, quis tenetur temporibus saepe vel similique expedita rerum accusamus eum est debitis asperiores animi atque, quod odit! Atque deleniti itaque voluptates nemo.',
+        ],
+        desc: {
+            author : 'Google and community',
+            initial_release: 'Alpha (v0.0.6)',
+            platform: 'Android, iOS',
+            license: 'New BSD License'
+        },
+        documentation: 'http://doc.com'
+    },
+]
+
+export default SkillData;
--- src/components/contents/skill/SkillDetail.jsx
+++ src/components/contents/skill/SkillDetail.jsx
@@ -4,13 +4,15 @@ import {Link, useParams} from "react-router-dom";
 import { withRouter } from "react-router";
 import Footer from '../footer/Footer';
 
+import SkillData from './SkillData';
+
 import '../Content.css';
 import './SkillDetail.css';
 
-// Image
-import flutter from '../../../assets/technology/flutter.png';
-
 class SkillDetail extends Component {
+    state = {
+        menu : this.props.match.params.title
+    }
 
     componentDidMount = () => window.scrollTo(0, 0);
 
@@ -19,59 +21,61 @@ class SkillDetail extends Component {
     };
 
     render() {
+        let data = SkillData.find( record => record.key === this.state.menu)
         return (
             <Fragment>
                 <Container className="content">
                     <div className="back">
                         <Link className="back-link" to="/"><i class="fas fa-long-arrow-alt-left"></i></Link>
                     </div>
-
-                    <div className="skill-content">
-                        <div className="tech-desc">
-                            <div className="title">
-                                <h3>{this.props.match.params.title}</h3>
-                            </div>
-                            <p>
-                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam blanditiis, ullam cum omnis quasi cumque maxime a tenetur nemo eveniet, culpa veniam adipisci dolorum atque non. Odit adipisci ab dolorum quod nostrum, illo similique quisquam libero recusandae aliquid non veritatis repudiandae, enim perspiciatis modi rem nobis aut magni eos excepturi?
-                            </p>
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit officia dignissimos, dicta sint laborum quo, magnam doloribus reprehenderit nisi asperiores cumque, veniam ad temporibus nobis blanditiis. Optio deserunt voluptate aperiam a nostrum dicta, similique enim eos unde alias ipsam non! Laborum odit exercitationem veritatis temporibus ex ipsa, voluptas non aliquid deleniti ipsum quae iste quisquam laboriosam dolor vero, illum cupiditate.
-                            </p>
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, repudiandae sed. Perferendis accusamus ab pariatur obcaecati magni? Non provident temporibus quibusdam iste alias harum a cupiditate corporis omnis exercitationem dicta aut repellat, ipsam unde?
-                            </p>
-                            <p>
-                                <a href="">See Documentation</a>
-                            </p>
-                        </div>
-
-                        <div className="tech-info">
-                            <img src={flutter} className="img-skill" alt="Skill Image" />
-
-                            <div className="detail-tech">
-                                <span className="text-bold detail-title">Author</span> 
-                                <span>Google and community</span>
-                            </div>
-                            <div className="detail-tech">
-                                <span className="text-bold detail-title">Initial release</span> 
-                                <span>Alpha (v0.0.6)</span>
-                            </div>
-                            <div className="detail-tech">
-                                <span className="text-bold detail-title">Platform</span> 
-                                <span>Android, iOS</span>
-                            </div>
-                            <div className="detail-tech">
-                                <span className="text-bold detail-title">License</span> 
-                                <span>New BSD License</span>
-                            </div>
-                            
-                        </div>
-                    </div>
+                    {data ? <ContentSkill tech={this.props.match.params.title} /> : <div></div>}
                 </Container>
                 <Footer />
             </Fragment>
         )
     }
+}
+
+const ContentSkill = (props)=>{
+    let data = SkillData.find( record => record.key === props.tech)
+    
+    return (
+        <div className="skill-content">
+            <div className="tech-desc">
+                <div className="title">
+                    <h3>{data.title}</h3>
+                </div>
+                {data.content.map((value, i) => {                   
+                    return (<p>{value}</p>) 
+                })}
+                <p>
+                    <a href={data.documentation}>See Documentation</a>
+                </p>
+            </div>
+
+            <div className="tech-info">
+                <img src={data.image} className="img-skill" alt="Skill Image" />
+
+                <div className="detail-tech">
+                    <span className="text-bold detail-title">Author</span> 
+                    <span>{data.desc.author}</span>
+                </div>
+                <div className="detail-tech">
+                    <span className="text-bold detail-title">Initial release</span> 
+                    <span>{data.desc.initial_release}</span>
+                </div>
+                <div className="detail-tech">
+                    <span className="text-bold detail-title">Platform</span> 
+                    <span>{data.desc.platform}</span>
+                </div>
+                <div className="detail-tech">
+                    <span className="text-bold detail-title">License</span> 
+                    <span>{data.desc.license}</span>
+                </div>
+                
+            </div>
+        </div>
+    )
 }
 
 export default withRouter(SkillDetail);
